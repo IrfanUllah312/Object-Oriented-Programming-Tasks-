70d3d8168045ef57f02741cbcf38e7565da5f005
Irfan Ullah <154457581+IrfanUllah312@users.noreply.github.com>
2025-07-15 10:54:56 -0700

    Add files via upload

---
+++ 04_0_Default_Values.cpp
@@ -0,0 +1,22 @@
+//#include"stdio.h"
+#include<iostream>
+//#include"iostream"
+using namespace std;
+int i;
+float f;
+char c;
+string s;
+
+int main()
+{
+    // int i; // Garbage
+    cout<<"\nValue is "<<i;
+    cout<<"\nValue is "<<f;
+    cout<<"\nValue is "<<c;
+    cout<<"\nValue is "<<int(c);
+    cout<<"\nValue is "<<s;
+    return 0;
+}  
+
+// Global has default values.
+// Local variables has garbage values 
